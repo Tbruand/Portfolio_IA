84625d2e11859071be7a4d72fbeea91400f4da32
Tbruand <tbruand.ia@gmail.com>
2025-01-12 18:58:59 +0100

    mise en forme du component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-const Portfolio = ({ tags, selectedTags, toggleTag, filteredProjects, onProjectClick }) => {
+const Portfolio = ({
+  tags,
+  selectedTags,
+  toggleTag,
+  filteredProjects,
+  onProjectClick,
+}) => {
   return (
     <section
-      id="portfolio"
-      className="min-h-screen pt-24 pb-12 bg-gray-100 dark:bg-gray-900 px-4 md:px-16"
+      id='portfolio'
+      className='min-h-screen pt-24 pb-12 bg-gray-100 dark:bg-gray-900 px-4 md:px-16'
     >
       {/* Titre de la section */}
-      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-8 text-gray-800 dark:text-gray-100">
+      <h2 className='text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-8 text-gray-800 dark:text-gray-100'>
         My Work
       </h2>
 
       {/* Filtres par tags */}
-      <div className="flex flex-wrap justify-center gap-4 mb-8">
+      <div className='flex flex-wrap justify-center gap-4 mb-8'>
         {tags.map((tag) => (
           <button
             key={tag}
@@ -29,28 +35,28 @@ const Portfolio = ({ tags, selectedTags, toggleTag, filteredProjects, onProjectC
       </div>
 
       {/* Grille des projets */}
-      <div className="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
+      <div className='max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4'>
         {filteredProjects.map((project) => (
           <div
             key={project.id}
-            className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md overflow-hidden transform hover:scale-105 transition-transform duration-300 cursor-pointer"
+            className='bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md overflow-hidden flex flex-col cursor-pointer'
             onClick={() => onProjectClick(project)}
           >
             {/* Image du projet */}
-            <div className="aspect-w-1 aspect-h-1">
+            <div className='flex-grow'>
               <img
                 src={project.image || "./assets/img/placeholder.webp"}
                 alt={project.title}
-                className="object-cover w-full h-full"
+                className='object-cover w-full h-full'
               />
             </div>
 
             {/* Détails du projet */}
-            <div className="p-4">
-              <h3 className="text-lg md:text-xl font-bold text-gray-800 dark:text-gray-100">
+            <div className='p-4 border-t h-28'>
+              <h3 className='text-lg md:text-xl font-bold text-gray-800 dark:text-gray-100'>
                 {project.title}
               </h3>
-              <p className="text-sm text-gray-600 dark:text-gray-400">
+              <p className='text-sm text-gray-600 dark:text-gray-400'>
                 Tags: {project.tags.join(", ")}
               </p>
             </div>
