6710a306a169885664d61ff533f4bc15fb26a013
Tbruand <tbruand.ia@gmail.com>
2025-01-19 19:10:19 +0100

    Ajout des couleurs pour le darkmode et les animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Inclut tous les fichiers React
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkmode: "#071E3F",
+        darkmode_500: "#071830",
+        darkmode_400: "#1a3868"        
+      },
+      animation: {
+        "fade-in": "fadeIn 0.3s ease-out",
+        "scale-in": "scaleIn 0.3s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        scaleIn: {
+          "0%": { transform: "scale(0.95)", opacity: 0 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        }
+      }
+    },
   },
   plugins: [
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/line-clamp'),
+    require('tailwind-scrollbar')
   ],
 };
